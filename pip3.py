e362a40f1cf8bd47ce75d1aa740df47eb7533bb9
shirha <sh1cpan@gmail.com>
2024-04-16 17:28:45 -0700

    initial commit

--- pip3.py
+++ pip3.py
@@ -48,10 +48,12 @@ beg_time = time.perf_counter()
 #, "expeditions\\expeditions11 voyagers" 2K
 dirs = ["normal\\normal8 orbital", "normal\\frontiers1", "normal\\normal5", "expeditions\\expeditions4", "normal\\normal6", "expeditions\\expeditions1 redux", "expeditions\\expeditions2 redux", "expeditions\\expeditions5 exobiology", "expeditions\\expeditions6 blighted", "expeditions\\expeditions7 leviathan", "expeditions\\expeditions8 polestar", "expeditions\\expeditions5 redux", "expeditions\\expeditions6 redux", "expeditions\\expeditions7 redux", "expeditions\\expeditions8 redux", "normal\\normal7", "expeditions\\expeditions9 utopia", "expeditions\\expeditions10 sigularity", "expeditions\\expeditions12 omega", "normal\\normal9", "derelict-restapi"] 
 
-for dir in ["normal\\playground"]: #["normal\\normal9"]: #["normal\\normal8 orbital"]: #["normal\\frontiers1"]: #["expeditions\\expeditions12 omega"]: #['expeditions\\expeditions7 redux']: #['expeditions\\expeditions6 redux']: #['normal\\normal8 orbital']: #['expeditions\\expeditions6 redux']: #["expeditions\\expeditions5 exobiology"]: #["normal\\frontiers1"]: #['normal\\playground']: #dirs: #filter(lambda x: title.lower() in x, dirs): # ["normal\\playground"]: #["derelict-restapi"]: #["normal\\normal9"]: #["normal\\playground"]: #["expeditions\\expeditions12 omega"]: #["normal\\normal8 omega"]: #['derelict-restapi']: #["normal\\normal9"]: #["expeditions\\expeditions9 utopia"]:  #dirs: #['orbital']: #["expeditions\\expeditions10 sigularity"]: #["normal\\normal8 orbital"]: 
-  for root, dirs, files in os.walk("\\Downloads\\No Man's Sky\\" + dir):
+for dir in ["normal\\normal9"]: #['.']: #dirs: #["normal\\playground"]: #["normal\\normal8 orbital"]: #["normal\\frontiers1"]: #["expeditions\\expeditions12 omega"]: #['expeditions\\expeditions7 redux']: #['expeditions\\expeditions6 redux']: #['normal\\normal8 orbital']: #['expeditions\\expeditions6 redux']: #["expeditions\\expeditions5 exobiology"]: #["normal\\frontiers1"]: #['normal\\playground']: #dirs: #filter(lambda x: title.lower() in x, dirs): # ["normal\\playground"]: #["derelict-restapi"]: #["normal\\normal9"]: #["normal\\playground"]: #["expeditions\\expeditions12 omega"]: #["normal\\normal8 omega"]: #['derelict-restapi']: #["normal\\normal9"]: #["expeditions\\expeditions9 utopia"]:  #dirs: #['orbital']: #["expeditions\\expeditions10 sigularity"]: #["normal\\normal8 orbital"]: 
+  for root, dirs, files in os.walk(dir): #"\\Downloads\\No Man's Sky\\" + dir):
     parts = root.split(os.sep)
-    if re.match(r'^\d$', os.path.basename(root)) and parts[-2] == 'py':
+    print(root)
+  if re.match(r'^\d$', os.path.basename(root)) and parts[-2] == 'py':
+  # if re.match(r'^\.\\\d$', root): # .\0 or .\1
       for file in files:
         path = f'{parts[-1]}/{file}'
         large_image = cv2.imread(os.path.join(root,file), cv2.IMREAD_GRAYSCALE)
